5214edbf04b0559abc62c4a61daf5f616d1562da
Jeferson Alves <jefersonalvessantos62@gmail.com>
2025-08-29 16:31:04 -0300

    Init: Atividade para dar os primeiros passos na lógica de Programação.

---
+++ nomeIdade.js
@@ -0,0 +1,8 @@
+/* Escreva um programa em que o usuário informe o seu nome e em seguida o programa perguntará a idade do
+ usuário.Agora o programa deve exibir a mensagem "Olá, [NomeDoUsuario], sua idade é [idade]". */
+
+ var nome = prompt("Olá seja bem-vindo, qual é o seu nome ? ");
+
+ var idade = parseInt(prompt("Qual é sua Idade : "));
+
+ alert("Olá " + nome + ", Sua idade é: "+ idade );
